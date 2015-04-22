9d85ca0699c9efab27c5e99171b6f38819fa9054
Patryk Pilek <patryk.pilek@gmail.com>
2015-04-23 00:37:14 +0100

    Passing Data to Views

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,18 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller {
+
+	public function about(){
+
+        $first = 'Patryk';
+        $last = 'Pilek';
+
+        return view('pages.about', compact('first', 'last'));
+    }
+
+}
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel5</title>
+</head>
+<body>
+    <h1>About Me {{ $first }} {{ $last }}</h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi debitis dicta dolorum ducimus molestias neque perferendis quaerat. Adipisci aperiam consequuntur facere ipsam, iusto labore sed totam. Debitis dignissimos id provident!
+    </p>
+</body>
+</html>

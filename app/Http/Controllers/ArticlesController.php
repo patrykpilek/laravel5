34c0df241350f32dd0e43a59cfe2da6fe2a3cc50
Patryk Pilek <patryk.pilek@gmail.com>
2015-04-23 02:30:28 +0100

    Basic Model/Controller/View Workflow

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,94 @@
+<?php namespace App\Http\Controllers;
+
+use App\Article;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+        $articles = Article::all();
+
+        return view('articles.index',compact('articles'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+//        $article = Article::find($id);
+        $article = Article::findOrFail($id);
+
+//        if(is_null($article)){
+//            abort(404);
+//        }
+
+        return view('articles.show', compact('article'));
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}

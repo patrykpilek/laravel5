f3121e082dab14cc5751703e94f1f1956d07b0a8
Patryk Pilek <patryk.pilek@gmail.com>
2015-04-23 01:29:45 +0100

    Migrations

--- database/migrations/2015_04_23_002440_add_excerpt_to_articles_table.php
+++ database/migrations/2015_04_23_002440_add_excerpt_to_articles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration {
+class AddExcerptToArticlesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,11 +12,9 @@ class CreatePasswordResetsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('password_resets', function(Blueprint $table)
+		Schema::table('articles', function(Blueprint $table)
 		{
-			$table->string('email')->index();
-			$table->string('token')->index();
-			$table->timestamp('created_at');
+			$table->text('excerpt')->nullable();
 		});
 	}
 
@@ -27,7 +25,10 @@ class CreatePasswordResetsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('password_resets');
+		Schema::table('articles', function(Blueprint $table)
+		{
+			$table->dropColumn('excerpt');
+		});
 	}
 
 }

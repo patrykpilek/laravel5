48a5b188b957c26f59e215db2946a40d1e4309fa
Patryk Pilek <patryk.pilek@gmail.com>
2015-04-25 01:42:16 +0100

    Eloquent Relationships

--- app/Article.php
+++ app/Article.php
@@ -5,26 +5,58 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model {
 
+    /**
+     * Fillable fields for an Article.
+     *
+     * @var array
+     */
 	protected $fillable = [
         'title',
         'body',
-        'published_at'
+        'published_at',
+        'user_id'       // Temporary
     ];
 
+    /**
+     * Additional fields to treat as Carbon instances.
+     *
+     * @var array
+     */
     protected $date = ['published_at'];
 
-    // scopeName
+    /**
+     * Scope queries to articles that have been published.
+     *
+     * @param $query
+     */
     public function scopePublished($query){
         $query->where('published_at', '<=', 'Carbon::now()');
     }
 
-    // scopeName
+    /**
+     * Scope queries to articles that have been published.
+     *
+     * @param $query
+     */
     public function scopeUnpublished($query){
         $query->where('published_at', '>', 'Carbon::now()');
     }
 
-    // setNameAttribute
+    /**
+     * Set the published_at attribute.
+     *
+     * @param $date
+     */
     public function setPublishedAtAttribute($date){
         $this->attributes['published_at'] = Carbon::createFromFormat('Y-m-d', $date);
+    }
+
+    /**
+     * An article is owned by user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+        return $this->belongsTo('App\User');
     }
 }
